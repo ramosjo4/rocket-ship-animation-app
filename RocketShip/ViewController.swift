f1ce69f33598edae0f10a44b07755362e0ce3d07
Jose Ramos <jar.arona@gmail.com>
2018-04-18 20:32:11 -0400

    working version

--- RocketShip/ViewController.swift
+++ RocketShip/ViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var DBBG: UIImageView!
+    @IBOutlet weak var newView: UIView!
+    @IBOutlet weak var Rocket: UIImageView!
+    @IBOutlet weak var Clouds: UIImageView!
+    @IBOutlet weak var blastOffLbl: UILabel!
+    
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -19,7 +27,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        DBBG.isHidden = true;
+        powerBtn.isHidden = true;
+        newView.isHidden = false;
+        Rocket.isHidden = false;
+        Clouds.isHidden = false;
+        
+        UIView.animate(withDuration: 3, animations: {
+            self.Rocket.frame = CGRect(x: 0, y: 150, width: 240, height: 128)
+        }){ (finished) in
+            self.blastOffLbl.isHidden = false;
+    }
+        
+    
 
 }
-
+}
